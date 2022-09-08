8f43da0aa03b3fcf8673cb1e4d18f22962787d88
10afrika <10@afrika.dev>
2022-09-08 20:57:49 +0300

    ESLint configuration

--- pages/index.jsx
+++ pages/index.jsx
@@ -63,7 +63,7 @@ const Home = () => {
               fetchNFTsForCollection()
             } else fetchNFTs()
           }
-        }>Let's go! </button>
+        }>Let&apos;s go! </button>
       </div>
       <div className='flex flex-wrap gap-y-12 mt-4 w-5/6 gap-x-2 justify-center'>
         {
