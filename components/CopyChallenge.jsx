62de5bf0a66185340e81edd6989ed48e23b4b15c
10afrika <10@afrika.dev>
2022-09-09 17:48:23 +0300

    Icon Challenge Fix

--- components/CopyChallenge.jsx
+++ components/CopyChallenge.jsx
@@ -1,16 +1,13 @@
 import { FiCopy } from 'react-icons/fi'
-import { FaCopy } from 'react-icons/fa'
-import React, { useState } from 'react';
+import React from 'react';
 
 
 const CopyChallenge = ({ content }) => {
-    const [active, setActive] = useState(false);
 
     return (
-        active ? <FaCopy onClick={() => setActive(!active)} /> : <FiCopy className='hover:text-indigo-400' onClick={
+        <FiCopy className='hover:text-indigo-400 active:text-white' onClick={
             () => {
                 navigator.clipboard.writeText(content)
-                setActive(!active)
             }
         } />
 
